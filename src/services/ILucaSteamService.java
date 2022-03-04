64de2b83e4620456d2aed69537eb833ec66b5971
Vieris <alex12293@gmail.com>
2022-03-04 14:41:01 +0100

    LucaSteamService (Interfaz y Clase)

---
+++ src/services/ILucaSteamService.java
@@ -0,0 +1,7 @@
+package services;
+
+public interface ILucaSteamService {
+	
+	public void mostrarJuegos();
+
+}
---
+++ src/services/LucaSteamService.java
@@ -0,0 +1,15 @@
+package services;
+
+public class LucaSteamService implements ILucaSteamService {
+	
+	//private IListado listado = new Listado();
+
+	@Override
+	public void mostrarJuegos() {
+		
+        // listado.mostrarJuegos();
+		
+    }
+	
+
+}
