319869c1963cfcb9758519b680faeeb454e73d7d
Vieris <alex12293@gmail.com>
2022-03-04 13:32:02 +0100

    clase Juego
    rectificación

--- src/model/Juego.java
+++ src/model/Juego.java
@@ -1,5 +1,12 @@
 package model;
+import lombok.Data;
 
-public class Juego {
+public @Data class Juego {
+	
+	private String nombre;
+	private String plataforma;
+	private int fechaPublicacion;
+	private String genero;
+	private String distribuidora;
 
 }
