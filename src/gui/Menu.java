28c24b1503cb3dc9dfd09932c25c18560daade72
xn0 <chanomm75@gmail.com>
2022-03-06 21:46:25 +0100

    Correción de codificación a UTF8

--- src/gui/Menu.java
+++ src/gui/Menu.java
@@ -19,10 +19,10 @@ public class Menu {
 
 	public void mostrarMenu(){
 		System.out.println("");
-		System.out.println(" Men� principal  ");
+		System.out.println(" Menú principal  ");
 		System.out.println("");
 		System.out.println("***************************");
-		System.out.println("Seleccione una opci�n: ");
+		System.out.println("Seleccione una opción: ");
 		System.out.println("	1. Listar juegos ");
 		System.out.println("	0. Salir ");	
 		System.out.println("***************************");
