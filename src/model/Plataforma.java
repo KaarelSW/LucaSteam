6c432f1b910d3b891bbb7cdc58b507ff69315dec
Vieris <alex12293@gmail.com>
2022-03-07 11:10:55 +0100

    update Plataforma
    añadidas empresa de cada consola

--- src/model/Plataforma.java
+++ src/model/Plataforma.java
@@ -1,35 +1,45 @@
 package model;
 
 public enum Plataforma {
-	WII,
-	NES,
-	SNES,
-	DS,
-	X360,
-	GB,
-	PS2,
-	PS4,
-	PS3,
-	N3DS,
-	XONE,
-	GC,
-	PS,
-	PSP,
-	GBA,
-	N64,
-	PC,
-	WIIU,
-	XB,
-	DOSMILSEISCIENTOS,
-	GEN,
-	SAT,
-	SCD,
-	WS,
-	TG16,
-	TRESDO,
-	NG,
-	DC,
-	GG,
-	PCFX,
-	PSV;
+	WII("Nintendo"),
+	NES("Nintendo"),
+	SNES("Nintendo"),
+	DS("Nintendo"),
+	X360("Microsoft"),
+	GB("Nintendo"),
+	PS2("Sony"),
+	PS4("Sony"),
+	PS3("Sony"),
+	N3DS("Nintendo"),
+	XONE("Microsoft"),
+	GC("Nintendo"),
+	PS("Sony"),
+	PSP("Sony"),
+	GBA("Nintendo"),
+	N64("Nintendo"),
+	PC("Microsoft"),
+	WIIU("Nintendo"),
+	XB("Microsoft"),
+	DOSMILSEISCIENTOS("Atari"),
+	GEN("Sega"),
+	SAT("Sega"),
+	SCD("Nintendo"),
+	WS("Bandai"),
+	TG16("NEC"),
+	TRESDO("3DO"),
+	NG("Nokia"),
+	DC("Sega"),
+	GG("Sega"),
+	PCFX("NEC"),
+	PSV("Sony");
+	
+	private final String empresa;
+
+    private Plataforma(String empresa) {
+        this.empresa = empresa;
+    }
+    
+    public String getEmpresa() {
+        return empresa;
+    }
 }
