e82e90a75783844352f762802516928d5177cd7f
Carlos Jesús Pérez Márquez <permarcar96@gmail.com>
2022-03-07 17:18:38 +0100

    Logger

---
+++ src/exceptions/LucaSteamExcepciones.java
@@ -0,0 +1,55 @@
+package exceptions;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+public class LucaSteamExcepciones extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	private Level level; 
+	
+	public LucaSteamExcepciones() {
+	}
+
+	public LucaSteamExcepciones(String message) {
+		super(message);
+	}
+
+	public LucaSteamExcepciones(Throwable cause) {
+		super(cause);
+	}
+
+	public LucaSteamExcepciones(String message, Throwable cause) {
+		super(message, cause);
+	}
+	public LucaSteamExcepciones(String message, int tipo) {
+		super(message);
+		setLevel(tipo);
+	}	
+
+	public void setLevel(int tipo) {
+
+		switch (tipo) {
+		case 1:
+			level = Level.SEVERE;
+			
+			break;
+		case 2:
+			level = Level.WARNING;
+			break;
+		case 3:
+			level = Level.INFO;
+			break;
+		default:
+			level = Level.INFO;
+			break;
+		}
+	}
+
+	@Override
+	public String toString() {
+		Logger.getLogger(LucaSteamExcepciones.class.getName()).log(level, null, super.getMessage());
+		return super.getMessage();
+	}
+}
