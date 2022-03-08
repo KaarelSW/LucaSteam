d337c0a18913a44224b1dd329487945a293ad79c
Vieris <alex12293@gmail.com>
2022-03-08 19:10:32 +0100

    docs

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"control","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"control","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
